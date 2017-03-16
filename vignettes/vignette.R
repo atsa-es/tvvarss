afb3345f64bc2c797ff75d8ab1f3a59b7718eb7e
eric-ward <eric.ward@noaa.gov>
2017-03-15 20:05:45 -0700

    pushed rebuild after marks commits

--- vignettes/vignette.R
+++ vignettes/vignette.R
@@ -17,13 +17,19 @@ y = array(rnorm(240), dim=c(3, 20, 4))
 #  tvvarss(y = y)
 
 ## ----function2, echo=TRUE, eval=FALSE------------------------------------
-#  tvvarss(y = y, include_trend = TRUE, x0 = NULL, shared_q = NULL, shared_r = NULL, shared_u = NULL, mcmc_iter = 1000, mcmc_warmup = 500, mcmc_thin = 1, mcmc_chain = 3)
+#  tvvarss(y = y, include_trend = TRUE, de_mean = TRUE, x0 = NULL, shared_q = NULL, shared_r = NULL, shared_u = NULL, mcmc_iter = 1000, mcmc_warmup = 500, mcmc_thin = 1, mcmc_chain = 3)
+
+## ---- echo=FALSE---------------------------------------------------------
+matrix(c(rep(0,4), rep(10,4), rep(5,4)), 4, 3)
 
 ## ---- echo=FALSE---------------------------------------------------------
 matrix(rep(1:7,3), 7, 3)
 
-## ------------------------------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------
 matrix(seq(1,7*3), 7, 3)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  tvvarss(y, shared_q = matrix(1, 4, 3), shared_r = matrix(1, 4, 3), shared_u = matrix(1, 4, 3))
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  plot(fitted_model, pars = c("B[3,2,2]", "u[1]"))
