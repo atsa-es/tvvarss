d779558728e0a2cd777c320935b5aebc4bf4b60b
mdscheuerell <mark.scheuerell@noaa.gov>
2017-04-04 15:41:01 -0700

    created sim2fit function

---
+++ R/sim2fit.R
@@ -0,0 +1,49 @@
+#' Convert simulated TVVARSS model to form suitable for fitting
+#'
+#' \code{sim2fit} adds observation error to a simulated TVVAR process and
+#'   converts it to a form suitable for fitting with \code{tvvarss}.
+#'
+#' This is a helper function that takes a fitted \code{simTVVAR} object and
+#' simulates multiple realizations of the process before adding Gaussian
+#' obsveration errors.
+#'
+#' @param \code{obj} A fitted \code{simTVVAR} object.
+#' @param \code{n_sim} The number of realizations of the TVVAR process.
+#' @param \code{sd} The standard deviation of the Gaussian observation errors.
+#'   Can be set to 0 for no observation error.
+#'
+#' @return An array with dimensions \code{c(n_sim, TT, n_spp)}.
+#'
+#' @examples
+#' set.seed(123)
+#' ## number of time steps
+#' TT <- 30
+#' ## number of spp/guilds
+#' nn <- 4
+#' ## CASE 1: linear food chain
+#' B0_lfc <- matrix(list(0),nn,nn)
+#' for(i in 1:(nn-1)) {
+#'   B0_lfc[i,i+1] <- "td"
+#'   B0_lfc[i+1,i] <- "bu"
+#' }
+#' ## simulate process
+#' lfc <- simTVVAR(B0_lfc,TT,var_QX=rev(seq(1,4)/40),cov_QX=0,var_QB=0.05,cov_QB=0)
+#' ## create data array with 3 realizations of the process
+#' dat <- sim2fit(lfc, 3)
+#'
+#' @export
+sim2fit <- function(obj, n_sim, sd=0.1) {
+  if(n_sim < 1 | round(n_sims) - n_sims != 0) {
+    stop("'n_sims' must be a positive integer.")
+  }
+  if(sd < 0) {
+    stop("'sd' must be non-negative.")
+  }
+  nC <- ncol(obj$states)-1
+  nR <- nrow(obj$states)
+  yy <- array(NA,c(n_sim,nC,nR))
+  for(i in 1:n_sim) {
+    y[i,,] <- t(eval(as.expression(obj$call))$states[,-1]) + matrix(rnorm(nC*nR,0,sd),nC,nR)
+  }
+  return(yy)
+}
--- R/simTVVAR.R
+++ R/simTVVAR.R
@@ -36,11 +36,12 @@
 #' @return A list with the following components:
 #' \describe{
 #' \item{\code{B_mat}}{An array of the \strong{B} matrix over time; \code{dim(B_mat) = c(n,n,T+1)}.}
-#' \item{\code{WW_BB}}{The process errors for \strong{B}.}
-#' \item{\code{QQ_BB}}{Variance-covariance matrix of the process errors for \strong{B}.}
+#' \item{\code{WW_BB}}{The process errors for \strong{B}; \code{dim(WW_BB) = c(n^2,T)}.}
+#' \item{\code{QQ_BB}}{Variance-covariance matrix of the process errors for \strong{B}; \code{dim(QQ_BB) = c(n^2,n^2)}.}
 #' \item{\code{states}}{A matrix of the states over time; \code{dim(states) = c(n,T+1)}.}
-#' \item{\code{WW_XX}}{The process errors (innovations) for the states.}
-#' \item{\code{QQ_XX}}{Variance-covariance matrix of the process errors for the states.}
+#' \item{\code{WW_XX}}{The process errors (innovations) for the states; \code{dim(WW_XX) = c(n,T)}.}
+#' \item{\code{QQ_XX}}{Variance-covariance matrix of the process errors for the states; \code{dim(QQ_XX) = c(n,n)}.}
+#' \item{\code{call}}{The function call as returned by \code{match.call()}.}
 #' }
 #'
 #' @examples
@@ -76,131 +77,114 @@
 #' matplot(t(cp2$states),type="l")
 #'
 #' @export
-simTVVAR <-
-  function(B0,
-    TT,
-    var_QX,
-    cov_QX,
-    var_QB,
-    cov_QB = 0,
-    QQ_XX = NULL,
-    QQ_BB = NULL,
-    X0 = NULL,
-    CC = NULL,
-    cc = NULL) {
-    if (class(B0) != "matrix" & class(B0) != "array") {
+simTVVAR <- function(B0, TT, var_QX, cov_QX, var_QB, cov_QB = 0,
+                     QQ_XX = NULL, QQ_BB = NULL, X0 = NULL,
+                     CC = NULL, cc = NULL) {
+  if (class(B0) != "matrix" & class(B0) != "array") {
+    stop("'B0' must be an [n x n] matrix or [n x n x T] array.\n\n")
+  } else {
+    if (length(dim(B0)) < 2 |
+        length(dim(B0)) > 3 | dim(B0)[1] != dim(B0)[2]) {
       stop("'B0' must be an [n x n] matrix or [n x n x T] array.\n\n")
-    } else {
-      if (length(dim(B0)) < 2 |
-          length(dim(B0)) > 3 | dim(B0)[1] != dim(B0)[2]) {
-        stop("'B0' must be an [n x n] matrix or [n x n x T] array.\n\n")
-      }
     }
-    ## number of spp/guilds
-    nn <- dim(B0)[1]
-    ## if no var-cov matrix for proc errors of states was passed, create one
-    if (is.null(QQ_XX)) {
-      ## var-cov matrix for proc errors of states
-      if (length(var_QX) > 1 & cov_QX != 0) {
-        stop("If 'var_QX is a vector, then 'cov_QX' must be 0.\n\n", call. = FALSE)
-      }
-      QQ_XX <- matrix(cov_QX, nn, nn)
-      diag(QQ_XX) <- var_QX
-    } else {
-      if (!all(dim(QQ_XX) == nn)) {
-        stop("'QQ_XX' must be an [n x n] matrix.\n\n")
-      }
+  }
+  ## number of spp/guilds
+  nn <- dim(B0)[1]
+  ## if no var-cov matrix for proc errors of states was passed, create one
+  if (is.null(QQ_XX)) {
+    ## var-cov matrix for proc errors of states
+    if (length(var_QX) > 1 & cov_QX != 0) {
+      stop("If 'var_QX is a vector, then 'cov_QX' must be 0.\n\n", call. = FALSE)
     }
-    ## if no var-cov matrix for proc errors of BB was passed, create one
-    if (is.null(QQ_BB)) {
-      ## var-cov matrix for proc errors of BB
-      if (length(var_QB) > 1 & cov_QB != 0) {
-        stop("If 'var_QB is a vector, then 'cov_QB' must be 0.\n\n", call. = FALSE)
-      }
-      QQ_BB <- matrix(cov_QB, nn * nn, nn * nn)
-      diag(QQ_BB) <- var_QB
-    } else {
-      if (!all(dim(QQ_BB) == nn)) {
-        stop("'QQ_BB' must be an [nn x nn] matrix.\n\n")
-      }
+    QQ_XX <- matrix(cov_QX, nn, nn)
+    diag(QQ_XX) <- var_QX
+  } else {
+    if (!all(dim(QQ_XX) == nn)) {
+      stop("'QQ_XX' must be an [n x n] matrix.\n\n")
     }
-    ## STATES
-    XX <- matrix(NA, nn, TT + 1)
-    ## initial states
-    if (is.null(X0)) {
-      XX[, 1] <- matrix(runif(nn, -5, 5), nn, 1)
-    } else {
-      XX[, 1] <- X0
+  }
+  ## if no var-cov matrix for proc errors of BB was passed, create one
+  if (is.null(QQ_BB)) {
+    ## var-cov matrix for proc errors of BB
+    if (length(var_QB) > 1 & cov_QB != 0) {
+      stop("If 'var_QB is a vector, then 'cov_QB' must be 0.\n\n", call. = FALSE)
     }
-
-    ## proc errors for states
-    WW_XX <- t(mvrnorm(TT, matrix(0, nn, 1), QQ_XX))
-    ## BB
+    QQ_BB <- matrix(cov_QB, nn * nn, nn * nn)
+    diag(QQ_BB) <- var_QB
+  } else {
+    if (!all(dim(QQ_BB) == nn)) {
+      stop("'QQ_BB' must be an [nn x nn] matrix.\n\n")
+    }
+  }
+  ## STATES
+  XX <- matrix(NA, nn, TT + 1)
+  ## initial states
+  if (is.null(X0)) {
+    XX[,1] <- matrix(runif(nn, -5, 5), nn, 1)
+  } else {
+    XX[,1] <- X0
+  }
+  ## proc errors for states
+  WW_XX <- t(mvrnorm(TT, matrix(0, nn, 1), QQ_XX))
+  ## BB
+  if (length(dim(B0)) == 2) {
+    ## then B0 is food web topology
+    ## initial BB
+    BB <- array(0, c(nn, nn, TT + 1))
+    ## build B0 based on topology
+    ## fill in diagonal
+    diag(BB[, , 1]) <- plogis(rnorm(nn, 0, 1))
+    ## fill in top-down interactions
+    i_td <- sapply(B0, function(x) {
+      x == "td"
+    })
+    BB[, , 1][i_td] <- -plogis(rnorm(sum(i_td), 0, 1))
+    ## fill in bottom-up interactions
+    i_bu <- sapply(B0, function(x) {
+      x == "bu"
+    })
+    BB[, , 1][i_bu] <- plogis(rnorm(sum(i_bu), 0, 1))
+    ## fill in competitive/facilitative interactions
+    i_cf <- sapply(B0, function(x) {
+      x == "cf"
+    })
+    BB[, , 1][i_cf] <- plogis(rnorm(sum(i_cf), 0, 1)) * 2 - 1
+    ## proc errors for BB
+    WW_BB <- t(mvrnorm(TT, matrix(0, nn*nn, 1), QQ_BB))
+    WW_BB[which(BB[,,1] == 0), ] <- 0
+  } else {
+    ## B0 is a passed [n x n x T] array of interaction strengths
+    BB <- B0
+    WW_BB <- NULL
+  }
+  ## covariates, if missing
+  if (is.null(CC)) {
+    CC <- matrix(0, nn, 1)
+    cc <- matrix(0, 1, TT + 1)
+  }
+  ## evolutions
+  for (t in 1:TT + 1) {
+    ## evolution of BB
     if (length(dim(B0)) == 2) {
-      ## then B0 is food web topology
-      ## initial BB
-      BB <- array(0, c(nn, nn, TT + 1))
-      ## build B0 based on topology
-      ## fill in diagonal
-      diag(BB[, , 1]) <- plogis(rnorm(nn, 0, 1))
-
-      ## fill in top-down interactions
-      i_td <- sapply(B0, function(x) {
-        x == "td"
-      })
-      BB[, , 1][i_td] <- -plogis(rnorm(sum(i_td), 0, 1))
-
-      ## fill in bottom-up interactions
-      i_bu <- sapply(B0, function(x) {
-        x == "bu"
-      })
-      BB[, , 1][i_bu] <- plogis(rnorm(sum(i_bu), 0, 1))
-
-      ## fill in competitive/facilitative interactions
-      i_cf <- sapply(B0, function(x) {
-        x == "cf"
-      })
-      BB[, , 1][i_cf] <- plogis(rnorm(sum(i_cf), 0, 1)) * 2 - 1
-      ## proc errors for BB
-      WW_BB <- t(mvrnorm(TT, matrix(0, nn * nn, 1), QQ_BB))
-      WW_BB[which(BB[, , 1] == 0), ] <- 0
-    } else {
-      ## B0 is a passed [n x n x T] array of interaction strengths
-      BB <- B0
-      WW_BB <- NULL
+      ## constrain diagonals to [0,1]
+      diag(BB[,,t]) <- plogis(qlogis(diag(BB[,,t-1])) + diag(matrix(WW_BB[,t-1],nn,nn)))
+      ## constrain top-down effects to [-1,0]
+      BB[,,t][i_td] <- -plogis(qlogis(-BB[,,t-1][i_td]) + WW_BB[i_td,t-1])
+      ## constrain bottom-up effects to [0,1]
+      BB[,,t][i_bu] <- plogis(qlogis(BB[,,t-1][i_bu]) + WW_BB[i_bu,t-1])
+      ## constrain comp-facil effects to [-1,1]
+      BB[,,t][i_cf] <- plogis(qlogis((BB[,,t-1][i_cf] + 1) / 2) + WW_BB[i_cf,t-1]) * 2 - 1
     }
-    ## covariates, if missing
-    if (is.null(CC)) {
-      CC <- matrix(0, nn, 1)
-      cc <- matrix(0, 1, TT + 1)
-    }
-    ## evolutions
-    for (t in 1:TT + 1) {
-      ## evolution of BB
-      if (length(dim(B0)) == 2) {
-        ## constrain diagonals to [0,1]
-        diag(BB[, , t]) <-
-          plogis(qlogis(diag(BB[, , t - 1])) + diag(matrix(WW_BB[, t - 1], nn, nn)))
-        ## constrain off-diag effects to [-1,1]
-        BB[, , t][i_td] <-
-          -plogis(qlogis(-BB[, , t - 1][i_td]) + WW_BB[i_td, t - 1])
-        BB[, , t][i_bu] <-
-          plogis(qlogis(BB[, , t - 1][i_bu]) + WW_BB[i_bu, t - 1])
-        BB[, , t][i_cf] <-
-          plogis(qlogis((BB[, , t - 1][i_cf] + 1) / 2) + WW_BB[i_cf, t - 1]) * 2 -
-          1
-      }
-      ## state
-      XX[, t] <-
-        BB[, , t] %*% (XX[, t - 1, drop = FALSE] - CC %*% cc[, t - 1]) + CC %*% cc[, t] + WW_XX[, t -
-            1]
-    }
-    return(list(
-      B_mat = BB,
-      WW_BB = WW_BB,
-      QQ_BB = QQ_BB,
-      states = t(XX),
-      WW_XX = WW_XX,
-      QQ_XX = QQ_XX
-    ))
-  } ## end function
+    ## state
+    XX[,t] <- BB[,,t] %*% (XX[,t-1,drop = FALSE] - CC %*% cc[,t-1]) + CC %*% cc[,t] + WW_XX[,t-1]
+  }
+  return(list(
+    B_mat = BB,
+    WW_BB = WW_BB,
+    QQ_BB = QQ_BB,
+    states = XX,
+    WW_XX = WW_XX,
+    QQ_XX = QQ_XX,
+    call = match.call()
+  ))
+} ## end function
